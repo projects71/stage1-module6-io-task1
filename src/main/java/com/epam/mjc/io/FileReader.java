d8a4f3292fd5f1621b9ec499a725330486d65d9e
Khusan <khusanabdukarimov007@gmail.com>
2023-05-15 19:55:25 +0500

    logger fixed

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -5,15 +5,15 @@ import java.util.logging.Logger;
 
 
 public class FileReader {
-    private final Logger LOGGER = Logger.getLogger(FileReader.class.getName());
+    private final Logger myLogger = Logger.getLogger(FileReader.class.getName());
 
     public Profile getDataFromFile(File file) {
 
         try(BufferedReader ignored = new BufferedReader(new java.io.FileReader(file))) {
-            LOGGER.info("check");
+            myLogger.info("check");
         }catch (IOException e) {
 
-            LOGGER.info(e.getMessage());
+           myLogger.info(e.getMessage());
             throw new MyOwnRuntimeException("My message");
         }
         return new Profile(file);
